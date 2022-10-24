8899b449399bdf3d3593ed39a5f91a77f7763e82
Kevin <kevintvu123@gmail.com>
2022-10-24 19:32:06 -0400

    added spotimages table

---
+++ backend/db/seeders/20221024231204-demo-spotimages.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('SpotImages', [
+      {
+        spotId: 1,
+        url: 'url 1',
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: 'url 2',
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: 'url 3',
+        preview: false
+      }
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('SpotImages', {
+      url: { [Op.in]: ['url 1', 'url 2', 'url 3'] }
+    }, {});
+  }
+};
