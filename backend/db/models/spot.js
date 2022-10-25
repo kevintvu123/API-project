fd780362828c39b21f96015f586b5149be793d44
Kevin <kevintvu123@gmail.com>
2022-10-24 20:05:50 -0400

    create bookings table

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -18,6 +18,10 @@ module.exports = (sequelize, DataTypes) => {
       Spot.hasMany(models.SpotImage, {
         foreignKey: 'spotId'
       })
+
+      Spot.hasMany(models.Booking, {
+        foreignKey: 'spotId'
+      })
     }
   }
   Spot.init({
