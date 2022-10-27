de701541b80dcf8106ebc769927f0340eecb0197
Kevin <kevintvu123@gmail.com>
2022-10-27 10:29:09 -0400

    Finished edit a booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -5,6 +5,8 @@ const { Spot, SpotImage, Review, Booking, ReviewImage } = require('../../db/mode
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { application } = require('express');
+const e = require('express');
 
 const router = express.Router();
 
@@ -55,9 +57,76 @@ router.get('/current', requireAuth, async (req, res) => {
     })
 })
 
+router.put('/:bookingId', requireAuth, async (req, res, next) => {
+    const { bookingId } = req.params
+    const { user } = req
+    const { startDate, endDate } = req.body
+
+    const startDateFormatted = new Date(startDate)
+    const endDateFormatted = new Date(endDate)
+
+    if (startDateFormatted > endDateFormatted) {
+        const err = Error("Validation error")
+        err.status = 400
+        err.errors = {
+            endDate: "endDate cannot come before startDate"
+        }
+        return next(err)
+    }
+
+    const findBooking = await Booking.findByPk(bookingId)
+
+    if (!findBooking) {
+        const err = Error("Booking couldn't be found")
+        err.status = 404
+        return next(err)
+    }
+
+    if (findBooking.userId !== user.id) {
+        const err = Error("Booking couldn't be found")
+        err.status = 404
+        return next(err)
+    }
+
+    if (findBooking.endDate < new Date) {
+        const err = Error("Past bookings can't be modified")
+        err.status = 403
+        return next(err)
+    }
 
 
+    if (findBooking.endDate === endDateFormatted && findBooking.startDate === startDateFormatted) {
+        const err = Error("Sorry, this spot is already booked for the specified dates")
+        err.status = 403
+        err.errors = {
+            startDate: "Start date conflicts with an existing booking",
+            endDate: "End date conflicts with an existing booking"
+        }
+        return next(err)
+    } else if (findBooking.endDate === endDateFormatted) {
+        const err = Error("Sorry, this spot is already booked for the specified dates")
+        err.status = 403
+        err.errors = {
+            endDate: "End date conflicts with an existing booking",
+        }
+        return next(err)
+    } else if (findBooking.startDate === startDateFormatted) {
+        const err = Error("Sorry, this spot is already booked for the specified dates")
+        err.status = 403
+        err.errors = {
+            startDate: "Start date conflicts with an existing booking",
+        }
+        return next(err)
+    }
 
+    findBooking.set({
+        startDate: startDate,
+        endDate: endDate
+    })
+    await findBooking.save()
+
+    res.json(findBooking)
+})
 
 
 module.exports = router;
