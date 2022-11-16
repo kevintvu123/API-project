5ae68cf9f984cafbc5e5c33a6b951d2a9d1d0477
Kevin <kevintvu123@gmail.com>
2022-11-16 10:13:16 -0500

    Create Review Store

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -2,12 +2,20 @@ import { csrfFetch } from "./csrf";
 
 //types
 const GET_SPOT_REVIEWS = '/reviews/getSpotReviews'
+const CREATE_SPOT_REVIEW = '/reviews/createSpotReview'
 
 //action creators
 const getSpotReviews = (reviews) => {
     return {
         type: GET_SPOT_REVIEWS,
         payload: reviews
+    }
+}
+
+const createSpotReview = (review) => {
+    return {
+        type: CREATE_SPOT_REVIEW,
+        payload: review
     }
 }
 
@@ -22,6 +30,20 @@ export const getSpotReviewsThunk = (spotId) => async dispatch => {
     }
 }
 
+export const createSpotReviewThunk = (review, spotId) => async dispatch => {
+    const response = await csrfFetch(`/api/spots/${spotId}/reviews`, {
+        method: "POST",
+        body: JSON.stringify(review)
+    })
+
+    if (response.ok) {
+        const createdReview = await response.json()
+
+        dispatch(createSpotReview(createdReview))
+        return response
+    }
+}
+
 const reviewsReducer = (state = {}, action) => {
     let newState = { ...state }
     switch (action.type) {
@@ -31,6 +53,9 @@ const reviewsReducer = (state = {}, action) => {
             allReviewsArr.forEach(review => allReviews[review.id] = review)
             newState['allReviews'] = { ...allReviews }
             return newState
+        case CREATE_SPOT_REVIEW:
+            const createdReview = action.payload
+            return createdReview
         default:
             return state;
     }
